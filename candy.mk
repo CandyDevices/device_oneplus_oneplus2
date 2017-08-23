dcdc1f79b90dce44fe67a107e3353f5bbb282a39
RJ Trujillo <rjtrujillo@linux.com>
2017-08-22 20:51:25 -0600

    op2: Something about Candy and rainbows

--- candy.mk
+++ candy.mk
@@ -19,13 +19,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oneplus2 device
 $(call inherit-product, device/oneplus/oneplus2/device.mk)
 
-# Inherit some common tipsy stuff.
-$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
+# Inherit some common Candy stuff.
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
 # Inherit oneplus2-specific vendor tree
 $(call inherit-product-if-exists, vendor/oneplus/oneplus2/oneplus2-vendor.mk)
 
-PRODUCT_NAME := tipsy_oneplus2
+PRODUCT_NAME := candy_oneplus2
 PRODUCT_DEVICE := oneplus2
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
